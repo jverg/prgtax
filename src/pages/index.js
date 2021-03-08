0fc3f47ab819bda38f4c20d68d56b973e11ea3bf
Jverg <giannisverg@gmail.com>
2021-03-08 21:37:12 +0200

    Service container initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import * as React from "react"
 // import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAddressCard, faCheck, faEnvelope, faFile, faPaste, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import './styles/index.scss'
 
 import Layout from "../components/layout"
@@ -28,6 +30,39 @@ const IndexPage = () => (
         />
       </Col>
     </Row>
+    <Container className="services-container">
+      <h2 className="services-container__header">Υπηρεσίες</h2>
+      <Row services-container-row>
+        <div className="col-sm-4 services-container-row-element">
+          <FontAwesomeIcon className="services-container-row-element__icon" icon={faAddressCard} size="2x" color="#77C5EF"/>
+          <p>φορολογικές δηλώσεις</p>
+        </div>
+        <div className="col-sm-4 services-container-row-element">
+          <FontAwesomeIcon className="services-container-row-element__icon" icon={faCheck} size="2x" color="#77C5EF"/>
+          <p>Τήρηση λογιστικών βιβλίων</p>
+        </div>
+        <div className="col-sm-4 services-container-row-element">
+          <FontAwesomeIcon className="services-container-row-element__icon" icon={faEnvelope} size="2x" color="#77C5EF"/>
+          <p>Σύνταξη ταμειακών ροών</p>
+        </div>
+        </Row>
+        <Row>
+          <div className="col-sm-4 services-container-row-element">
+            <FontAwesomeIcon className="services-container-row-element__icon" icon={faFile} size="2x" color="#77C5EF"/>
+            <p>Διαχείριση προσλήψεων</p>
+          </div>
+          <div className="col-sm-4 services-container-row-element">
+            <FontAwesomeIcon className="services-container-row-element__icon" icon={faPaste} size="2x" color="#77C5EF"/>
+            <p>Πληρωμές ασφαλιστικών εισφορών</p>
+          </div>
+          <div className="col-sm-4 services-container-row-element">
+            <FontAwesomeIcon className="services-container-row-element__icon" icon={faThumbsUp} size="2x" color="#77C5EF"/>
+            <p>
+              Έκδοση μισθοδοτικών καταστάσεων
+              </p>
+          </div>
+        </Row>
+      </Container>
   </Layout>
 )
 
