1917a99aac76f6ec2ccbfc4f03c656c847342fec
Jverg <giannisverg@gmail.com>
2021-04-08 14:23:57 +0300

    Texts refactor

--- src/components/header/header.el.js
+++ src/components/header/header.el.js
@@ -35,8 +35,10 @@ const Header = ({ siteTitle, lang}) => (
           <Nav>
             <Nav.Link className="menu-item" href="/">Αρχική</Nav.Link>
             <NavDropdown className="menu-item" title="Υπηρεσίες" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/accounting">Φορολογικό-Λογιστικό</NavDropdown.Item>
-              <NavDropdown.Item href="/payroll">Payroll</NavDropdown.Item>
+              <NavDropdown.Item href="/accounting">Φορολογικές - Λογιστικές</NavDropdown.Item>
+              <NavDropdown.Item href="/payroll">Συμβουλευτικές Υπηρεσίες</NavDropdown.Item>
+              <NavDropdown.Item href="/payroll">Μισθοδοσία</NavDropdown.Item>
+              <NavDropdown.Item href="/payroll">Νομικές Υπηρεσίες</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="/contact">Επικοινωνία</Nav.Link>
           </Nav>
