f5f6d962074aa284c243cc9174cb0c8e47524578
Jverg <giannisverg@gmail.com>
2021-05-11 10:17:22 +0300

    Translate index and consultant

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -20,8 +20,12 @@ const IndexPage = () => (
             alt="PRG logo"
             className="logo"
           />
-        </h3>
-        <h5>Accounting - Consulting office</h5>
+        </h3><br></br>
+        <h4>
+          “The hardest thing in the world <br></br>to understand is the income tax.”
+          <br></br><br></br>
+          Albert Einstein
+        </h4>
       </Col>
       <Col lg="6" md="6" sm="12" xs="12" className="first-index-row-img-col">
         <div className="first-index-row-img-col__main-image" />
@@ -63,14 +67,22 @@ const IndexPage = () => (
         <div className="team-row-img-col__main-image" />
       </Col>
       <Col lg="6" md="6" sm="12" xs="12" className="team-row-text-col">
-        <h5 className="team-row-text-col__text">
-          We are one of the largest Accounting - Tax offices nationwide.
-          Through a large team of experienced accountants we try to stand by and help him
-          taxpayer through modern solutions and alternative procedures
-          in order to face its tax and labor burdens. Your
-          We are waiting at the accounting office to meet and solve your problems
-          problems up close.
-        </h5>
+        <h6 className="team-row-text-col__text">
+          Businesses look for explicit, comprehensible and updated business consultation and
+          consistent support of their needs and set targets.<br></br><br></br>
+          <b className="team-row-text-col__text-bold">PRG ACCOUNTING & CONSULTING</b>
+          is a Tax Accounting Consultants Company, with its head office in the business 
+          center of Athens, that offers complete tax services over a wide and diverse 
+          spectrum of companies, industries and individuals all over Greece.<br></br><br></br>
+          <b>Our purpose is for our clients to:</b><br></br><br></br>
+          <b className="team-row-text-col__text-bold">Make Profit: </b>
+          We can help businesses to expand their margin of profit keeping their financial data updated 
+          and well-presented. Businesses maximize their income and take better decision, based in 
+          our daily, weekly or monthly reports.<br></br><br></br>
+          <b className="team-row-text-col__text-bold">Save Time: </b>
+          While we keep the books of our clients, Clients save their precious time focusing on more 
+          productive business activities.
+        </h6>
       </Col>
     </Row>
   </LayoutEn>
