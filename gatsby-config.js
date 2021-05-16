30e8dd1122b2facc24230455a3a79a06ec2990e0
Jverg <giannisverg@gmail.com>
2021-05-16 19:07:28 +0300

    Remove gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
-    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
